86dc79a01bd260686203308c3aba73bba6c2dd3b
Steven Haryanto <stevenharyanto@gmail.com>
2012-01-20 09:00:46 +0000

    Import of SHARYANTO/Sub-Spec-GetArgs-GetPost-0.06 from CPAN.
    gitpan-cpan-distribution: Sub-Spec-GetArgs-GetPost
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         SHARYANTO/Sub-Spec-GetArgs-GetPost-0.06.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Sub/Spec/GetArgs/GetPost.pm
+++ lib/Sub/Spec/GetArgs/GetPost.pm
@@ -15,7 +15,7 @@ use Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(get_args_from_getpost);
 
-our $VERSION = '0.05'; # VERSION
+our $VERSION = '0.06'; # VERSION
 
 our %SPEC;
 
@@ -206,7 +206,7 @@ Sub::Spec::GetArgs::GetPost - Get subroutine arguments from HTTP GET/POST reques
 
 =head1 VERSION
 
-version 0.05
+version 0.06
 
 =head1 SYNOPSIS
 
@@ -215,6 +215,12 @@ version 0.05
  my $res = get_args_from_getpost(psgi_env=>$env, spec=>$spec, ...);
 
 =head1 DESCRIPTION
+
+B<NOTICE>: This module and the L<Sub::Spec> standard is deprecated as of Jan
+2012. L<Rinci> is the new specification to replace Sub::Spec, it is about 95%
+compatible with Sub::Spec, but corrects a few issues and is more generic.
+C<Perinci::*> is the Perl implementation for Rinci and many of its modules can
+handle existing Sub::Spec sub specs.
 
 This module provides get_args_from_getpost(), which parses HTTP GET/POST request
 data into subroutine arguments (%args).
@@ -308,7 +314,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Steven Haryanto.
+This software is copyright (c) 2012 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
